90dabba40b0e0393e5075b10f5055b62834cca13
samson meseret <samsonmeseretb@gmail.com>
2022-04-10 07:17:42 -0700

    working on resourse model

--- model/resource.js
+++ model/resource.js
@@ -1,3 +1,22 @@
 const mongoose = require("mongoose");
 
-// const resourceSchema = new mongoose.Schema({});
+const resourceSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: [true, "Please provide expalnation for the resource"],
+    },
+    fileOne: {
+      type: String,
+    },
+    fileTwo: {
+      type: String,
+    },
+    fileThree: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Resource", resourceSchema);
