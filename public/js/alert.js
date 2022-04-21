8497b98eda9ec982cff6bc88342c00cb2d657221
samson meseret <samsonmeseretb@gmail.com>
2022-04-21 11:45:04 -0700

    css update

--- public/js/alert.js
+++ public/js/alert.js
@@ -5,9 +5,9 @@ const hideAlert = () => {
 
 //type is success or error
 
-const showAlert = (type, msg) => {
+export const showAlert = (type, msg) => {
   hideAlert();
-  const markup = `<div> class="alert ${type}">${msg}</div>`;
-  document.querySelector("body").insertAdjacentElement("afterbegin", markup);
+  const markup = `<div class="alert alert-${type}">${msg}</div>`;
+  document.querySelector("body").insertAdjacentHTML("afterbegin", markup);
   window.setTimeout(hideAlert, 5000);
 };
